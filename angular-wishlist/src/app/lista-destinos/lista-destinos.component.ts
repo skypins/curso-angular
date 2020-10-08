abd5ccf2a74b7936b9f92eb8659e8eab679dbd19
Marcelo Bravo <correomhb@hotmail.com>
2020-10-08 19:19:53 -0300

    fin modulo 1

---
+++ angular-wishlist/src/app/lista-destinos/lista-destinos.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { DestinoViaje } from '../models/destino-viaje.model';
+
+@Component({
+  selector: 'app-lista-destinos',
+  templateUrl: './lista-destinos.component.html',
+  styleUrls: ['./lista-destinos.component.css']
+})
+export class ListaDestinosComponent implements OnInit {
+  destinos: DestinoViaje[];
+  constructor() {
+    this.destinos = [];
+   }
+
+  ngOnInit(): void {
+  }
+
+  guardar (nombre:string, url:string):boolean {
+    this.destinos.push(new DestinoViaje(nombre, url)); //con push vamos agregando los elementos en el array de destinos
+    console.log(this.destinos);
+    return false;
+  }
+
+}
