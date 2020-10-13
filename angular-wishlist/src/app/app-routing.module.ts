007d2234e9d8d31b1f64e13cc366e41f7296c032
Marcelo Bravo <correomhb@hotmail.com>
2020-10-12 22:35:44 -0300

    agregamos routing basico

--- angular-wishlist/src/app/app-routing.module.ts
+++ angular-wishlist/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DestinoDetalleComponent } from './destino-detalle/destino-detalle.component';
+import { ListaDestinosComponent } from './lista-destinos/lista-destinos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: ListaDestinosComponent},
+  { path: 'destino', component: DestinoDetalleComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-wishlist/src/app/app.module.ts
+++ angular-wishlist/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DestinoViajeComponent } from './destino-viaje/destino-viaje.component';
 import { ListaDestinosComponent } from './lista-destinos/lista-destinos.component';
+import { DestinoDetalleComponent } from './destino-detalle/destino-detalle.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     DestinoViajeComponent,
-    ListaDestinosComponent
+    ListaDestinosComponent,
+    DestinoDetalleComponent
   ],
   imports: [
     BrowserModule,
